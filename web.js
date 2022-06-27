16d59e7f150fc59df2e48a51b852f0417f7585ed
kimmensan <iq156mensan@gmail.com>
2022-06-28 00:48:27 +0900

    2

--- web.js
+++ web.js
@@ -1,31 +1,18 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-const getHtml = async () => {
-    try {
-        return await axios.get('https://maplestory.nexon.com/Common/Guild?gid=12458&wid=46');
-    } catch (error) {
-        console.error(error);
-    }
-};
+
+
+
+
 
 
 async function test() {
-    var test = await getHtml()
-    var $ = cheerio.load(test.data)
-    var table = $('div.guild_user_list table.rank_table tbody tr')
 
-    let ulList = [];
-    console.log(table[0])
-    table.each(function (i, elem) {
-        ulList[i] = {
-            nickname: $(this).find('td.left dl dt a').text(),
-            Job: $(this).find('td.left dl dd').text(),
-            LV: $(this).find('td:nth-of-type(3)').text().slice(3)
-        };
-    });
+    let driver = await new webdriver.Builder().forBrowser(webdriver.Browser.FIREFOX).build();
 
-    for (var i = 0; i < 10; i++)
-        console.log(ulList[i])
+    driver.get("https://maplestory.nexon.com/Authentication/Login#a")
 }
+
+
 test()
